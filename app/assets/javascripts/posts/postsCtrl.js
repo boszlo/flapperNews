a4b276874929d93148b4c8b011d3672e59edde99
boszlo <maciej.x.deptula@gmail.com>
2016-12-11 20:14:20 +0000

    angular-rails-templates added + restructured

---
+++ app/assets/javascripts/posts/postsCtrl.js
@@ -0,0 +1,17 @@
+angular.module('flapperNews').controller('PostsCtrl', [
+  '$scope',
+  '$stateParams',
+  'posts',
+  function($scope, $stateParams, posts){
+    $scope.post = posts.posts[$stateParams.id];
+
+    $scope.addComment = function() {
+      if($scope.body === '') { return; }
+      $scope.post.comments.push({
+        body: $scope.body,
+        upvotes: 0,
+        author: 'user'
+      });
+      $scope.body = '';
+    };
+}]);
